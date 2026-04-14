b94fb4013f9a2dac681c5b3bfa677b0e86ac99a7
mbeka02 <antonymbeka@gmail.com>
2026-04-14 12:21:45 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
+
+func handleConnection(conn net.Conn) {
+	defer func() {
+		log.Println("...closing the connection")
+		conn.Close()
+	}()
+	log.Println("...handling connection from:", conn.RemoteAddr())
+	resp := "+PONG\r\n"
+	_, err := conn.Write([]byte(resp))
+	if err != nil {
+		fmt.Println("Erroor writing bytes over the TCP connection")
+		// TODO: Handle this differently don't just crash the program
+		os.Exit(1)
+	}
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
@@ -12,16 +28,13 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	resp := "+PONG\r\n"
-	_, err = conn.Write([]byte(resp))
-	if err != nil {
-		fmt.Println("Erroor writing bytes over the TCP connection")
-		// TODO: Handle this differently don't just crash the program
-		os.Exit(1)
+	defer listener.Close()
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleConnection(conn)
 	}
 }
